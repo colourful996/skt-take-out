f9e4d9a08c10950eea3ca89aed787b984762efb0
sy <y81918798@163.com>
2024-03-10 22:46:47 +0800

    新增菜品业务代码开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,36 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+/**
+ * 通用接口
+ */
+@RestController
+@RequestMapping("/admin/common")
+@Api(tags = "通用接口")
+@Slf4j
+public class CommonController {
+    @PostMapping("/upload")
+    @ApiOperation("文件上传")
+    public Result<String> upload(MultipartFile file) {
+        log.info("文件上传:", file);
+
+        String originalFilename = file.getOriginalFilename();
+        //截取原始文件名
+        String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String uuName = UUID.randomUUID().toString()+extension;
+        /**
+         *  TODO,把上传的文件保存到本地，并返回本地存储的绝对路径
+         */
+        return Result.success();
+    }
+}
